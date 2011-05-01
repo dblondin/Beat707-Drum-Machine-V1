8e4b37c0d6af6ca6bf8f34dc6da4d1e91e8e53b3
Beat707 <youtube@beat707.com>
2011-05-01 16:28:55 -0300

    New Doc files

--- Config.h
+++ Config.h
@@ -14,10 +14,15 @@
   #define MAXSPATTERNS 90       // Check W_Storage to see the size of each pattern, so you know how many patterns you can store on an EEPROM chip
   #define MAXSONGSFILE 21       // Used by the Flash Storage, to determinate how many songs the Flash memory can hold (see W_Storage)
   #define MIDIECHO 1            // Copies all Midi Input to the Midi Output
-  
-  // ===================================================================================================================================================================== //
+
+  // ===================================================================================================================================================================== // 
+  // List of possible Hacks and Mods
   #define ANALOG_INPUT_A0 1     // Reads the analog input A0 (D14 on the Beat707 SV2 Headers) for multiple options (also uses ANALOG_PATT_MAX and ANALOG_MDLY below)
   #define ANALOG_INPUT_CHECK 0  // When Enabled in conjunction with ANALOG_INPUT_A0, it will only work if a button is attached and pressed on Digital Pin 2 (S2 on Beat707 SV3 Headers) Pull-Up Resistors internally used, so all you need to do is attach a regular button to the port and connect the button to ground.
+  #define ANALOG_PATT_MAX 16    // If Analog Input is enabled and is in Pattern mode, this will define the max number of patterns to select. (0 to MAX)
+  #define ANALOG_MDLY 100       // If Analog Input is enabled, the delay for when a new mode is selected (in ms) or, if ANALOG_INPUT_CHECK is enabled, the time to pause when the button is pressed 
+    
+  // ===================================================================================================================================================================== //
   #define CHECK_FOR_USB_MODE 0  // The Device will check if the USB Remote Program is running (takes 1 second during initialization)
   #define EXTENDED_DRUM_NAMES 1 // Add more GM Drum Note Names to the Track Drum Note Selectors
   #define STORAGE_FORCE_INIT 0  // Force an Initiation of all EEPROM memory during startup 
@@ -30,8 +35,6 @@
   #define DISABLE_STORAGE_CHK 0 // Debug Only
   #if ANALOG_INPUT_A0
     #define LAST_PATT_ZONE 14   // The code of the last Pattern Zone. If you add a new option/zone, you will need to update this value.
-    #define ANALOG_PATT_MAX 16  // If Analog Input is enabled and is in Pattern mode, this will define the max number of patterns to select. (0 to MAX)
-    #define ANALOG_MDLY 100     // If Analog Input is enabled, the delay for when a new mode is selected (in ms) or, if ANALOG_INPUT_CHECK is enabled, the time to pause when the button is pressed
   #else
     #define LAST_PATT_ZONE 13
   #endif
